a5fa7d4f96d2b80fe1bedd83ac6a272fa7beee91
Kazuma Arino <hogeika2@gmail.com>
2025-11-04 10:10:59 +0900

    Fix copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,10 +3,10 @@
 SUBWIKI_NAME="Biochemistry705x"
 
 SCRIPTS_DIR=$(dirname $0)
-SYNCTHING_DIR=$1
+TEFWIKI_DIR=$1/TeFWiki
 DEST_SRC="${SCRIPTS_DIR}/../wiki_src"
 DEST_RES="${SCRIPTS_DIR}/../wiki"
 
 rm $DEST_SRC/*
 rm $DEST_RES/*
-cp ${SYNCTHING_DIR}/${SUBWIKI_NAME}/*.md $DEST_SRC/
+cp ${TEFWIKI_DIR}/${SUBWIKI_NAME}/*.md $DEST_SRC/
