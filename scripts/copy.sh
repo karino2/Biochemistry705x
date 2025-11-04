cf676b1058c10bd866e28dad33b74ba1a7d6c891
Kazuma Arino <hogeika2@gmail.com>
2025-11-04 10:08:13 +0900

    Parametrize syncthing dir.

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,9 +3,10 @@
 SUBWIKI_NAME="Biochemistry705x"
 
 SCRIPTS_DIR=$(dirname $0)
+SYNCTHING_DIR=$1
 DEST_SRC="${SCRIPTS_DIR}/../wiki_src"
 DEST_RES="${SCRIPTS_DIR}/../wiki"
 
 rm $DEST_SRC/*
 rm $DEST_RES/*
-cp ~/work/syncthing_dirs/TeFWiki/${SUBWIKI_NAME}/*.md $DEST_SRC/
+cp ${SYNCTHING_DIR}/${SUBWIKI_NAME}/*.md $DEST_SRC/
